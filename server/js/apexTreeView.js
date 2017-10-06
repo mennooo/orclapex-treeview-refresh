4c6040d3e2cc8aa9432622f6951a66df6270bcf4
mennooo <menno.hoogendijk@iadvise.eu>
2017-10-06 18:18:55 +0200

    V1.1

--- server/js/apexTreeView.js
+++ server/js/apexTreeView.js
@@ -1,9 +1,21 @@
 /* global apex $ */
 window.mho = window.mho || {}
 ;(function (namespace) {
+  // We need to extend the treeView widget to return the baseId
+  $.widget('apex.treeView', $.apex.treeView, {
+    mhoGetBaseId: function () {
+      return this.baseId
+    }
+  })
+  // Keep all tree configurations in memory, useful on creating a new instance
+  var gConfigurations = []
+
   function refresh (options) {
+    // Get the widget element
     var tree$ = options.da.affectedElements.find('.a-TreeView')
-    var nodeAdapter = tree$.treeView('getNodeAdapter')
+    // Get the config from memory, identified by regionId
+    var config = gConfigurations[options.da.affectedElements.first().attr('id')]
+    // Get new tree data
     var promise = apex.server.plugin(options.ajaxIdentifier, {
       pageItems: options.itemsToSubmit
     }, {
@@ -13,9 +25,27 @@ window.mho = window.mho || {}
 
     var expandedNodes,
       selectedNodes
-
+    // After AJAX, refresh tree
     promise.done(function (data) {
-      nodeAdapter.data = data
+      // What to do when tree is empty after refresh
+      if ($.isEmptyObject(data.data)) {
+        tree$.trigger('apexafterrefresh')
+        apex.da.resume(options.da.resumeCallback, false)
+        tree$.treeView('destroy')
+        return
+      }
+
+      // Create instance of tree if it does not exist
+      if (tree$.length === 0) {
+        apex.widget.tree.init(config.treeId, {}, data.data, config.treeAction, config.selectedNodeId, data.config.hasIdentity, data.config.rootAdded, config.hasTooltips, config.iconType)
+        apex.da.resume(options.da.resumeCallback, false)
+        return
+      }
+
+      // If tree still exists then refresh data
+      var nodeAdapter = tree$.treeView('getNodeAdapter')
+
+      nodeAdapter.data = data.data
       expandedNodes = tree$.find('.is-collapsible').toArray()
       selectedNodes = tree$.treeView('getSelection').toArray()
 
@@ -39,7 +69,12 @@ window.mho = window.mho || {}
     })
   }
 
+  function addInitConfig (config) {
+    gConfigurations[config.regionId.toString()] = config
+  }
+
   namespace.apexTreeView = {
-    refresh: refresh
+    refresh: refresh,
+    addInitConfig: addInitConfig
   }
 })(window.mho)
